8079fd6ea340bd3980db65163133423e0332d022
changkaijie <changkaijie@core-tech.tw>
2020-07-15 17:47:42 +0800

    +Jest , Puppeteer

--- nodejs/basic.test.js
+++ nodejs/basic.test.js
@@ -5,3 +5,30 @@ basic check
     console.log
     all ajax 200
  */
+const puppeteer = require('puppeteer');
+
+const test_domain = "https://www.google.com";
+
+//  just for sample
+describe.each([
+    ['/', ''],
+])(test_domain, (testUrl, text) => {
+    var browser, page;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        // await page.emulate(iPhonex);
+        // await page.authenticate({username: user, password: pass});
+        await page.goto(test_domain + testUrl, {waitUntil: 'load', timeout: 0});
+    });
+
+    afterAll(() => {
+        browser.close()
+    });
+
+    it('should be titled "Google"', async () => {
+        await expect(page.title()).resolves.toMatch('Google');
+    });
+
+});
