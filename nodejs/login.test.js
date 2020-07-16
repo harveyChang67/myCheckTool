7421bb1ffc97dcdaafbe403634e7c7963203e1cb
changkaijie <changkaijie@core-tech.tw>
2020-07-16 10:12:15 +0800

    +plan for login.test

---
+++ nodejs/login.test.js
@@ -0,0 +1,9 @@
+//  login
+/*
+check if login page：頁面正確、是否已登入、欄位正確？
+完全沒有 keyin -> submit    =>  do what? error msg, no redirect....
+keyin 資料 格式檢查：缺一、必填...
+login 成功
+ */
+const puppeteer = require('puppeteer');
+
